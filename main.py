2da97b87e90e7af6af31e0a261c8317b71537e39
kigh350 <kaltenbergk@gmail.com>
2020-04-05 19:13:21 +0200

    zad3

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from typing import Dict
-
 from fastapi import FastAPI
-
 from pydantic import BaseModel
 
 
@@ -27,4 +25,20 @@ def read_item():
 @app.put("/method")
 def read_item():
     return {"method": "PUT"}
+
+class imie_nazwisko(BaseModel):
+    name: str
+    surename: str
+
+N=0
+@app.post("/patient", response_model=imie_nazwisko)
+def read_item(rq: imie_nazwisko):
+    N=N+1
+    return {"id": N, "patient": {"name": imie_nazwisko.name, "surename": imie_nazwisko.surname}}
+
+
+
+
+
+
 
