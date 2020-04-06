fa9768cac53cf4b5a6b92d2853944ab86c30a975
kigh350 <kaltenbergk@gmail.com>
2020-04-06 15:59:14 +0200

    zad4

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException
 
 from pydantic import BaseModel
 #import sqlite3
-import os
+#import os
 app = FastAPI()
 
 
@@ -27,45 +27,36 @@ def read_item():
 def read_item():
     return {"method": "PUT"}
 
-
 class im_nazw(BaseModel):
-    name: str
-    surename: str
+    name: str 
+    surename: str = "SURENAME"
 
+patient = im_nazw(
+    name="NAME",
+    surename="SURENAME"
+)
 
 class im_nazw_Resp(BaseModel):
     id: int
     patient: Dict
-    
-class im_nazw_pat(BaseModel):
-    Dict    
- 
+
 n=0
-slownik_id = {}
+lista=[n]
 @app.post("/patient", response_model=im_nazw_Resp)
 def receive_patient(rq: im_nazw):
-    global slownik_id
     global n
-    slownik_id = {n: rq}
+    global wynik
+    global lista
+    n += 1
+    lista[n] = n
     slownik = im_nazw_Resp(id=n, patient=rq.dict())
     wynik = slownik.dict()
-    n += 1
     return wynik
 
-class HelloResp(BaseModel):
-    msg: str
-
 @app.get("/patient/{pk}")
-async def receive_patient(pk: int):
-    if(pk in slownik_id.keys()):
-        return slownik_id[pk]
-    #else: 
-        #raise HTTPException(status_code=204) 
-
-
-
-
-
-
-
-
+def read_item(pk: int):
+    if pk not in lista:
+        raise HTTPException(status_code=204, detail="Item not found")
+    else:
+        results = patient
+    return results
